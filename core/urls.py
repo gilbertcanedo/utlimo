feecccd715994fbaaa73ddbb18ab908c4a81dd65
gilbertcanedo <143674302+gilbertcanedo@users.noreply.github.com>
2025-06-02 20:54:24 -0500

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    
+    path('store/', views.store, name="store"),
+    path('contact/', views.contact, name="contact"),
+    
+    
+
+    #url de aplicaciones
+
+    
+]
